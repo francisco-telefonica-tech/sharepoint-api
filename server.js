86e1438bb48f35741b47d1d518aaa97b40911964
FRANCISCO JIMENEZ MARTIN <francisco.jimenezmartin@telefonica.com>
2024-11-28 12:28:33 +0100

    Format code refactor

--- server.js
+++ server.js
@@ -1,117 +1,141 @@
-const express = require('express')
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
-const cors = require('cors');
-const SharepointService = require('./sharepoint-service');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const cors = require("cors");
+const SharepointService = require("./sharepoint-service");
 
 class Server {
-    
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.router = express.Router();
+  constructor() {
+    this.app = express();
 
-        // Middlewares
-        this.middelwares();
+    this.port = process.env.PORT;
+    this.router = express.Router();
 
-        // pendiente de implementacion
-        // this.sharepointService = new SharepointService();
+    // Middlewares
+    this.middelwares();
 
-        // Rutas de la aplicacion
-        this.routes();
-    }
+    // pendiente de implementacion
+    // this.sharepointService = new SharepointService();
 
-    middelwares() {
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(express.json());
-        this.app.use('/api', this.router);
-        this.app.use(cors());
-        this.router.use( (req, res , next) => { next() });
-        this.router.use(fileUpload({
-            limits: { fileSize: 50 * 1024 * 1024 },
-            useTempFiles : true,
-            tempFileDir : '/tmp/'
-        }));
-    }
+    // Rutas de la aplicacion
+    this.routes();
+  }
 
-    routes() {
-        // ENDPOINT -> CREATE FOLDE IN SHAREPOINT
-        this.router.get('/create-folder/:name?',  (req = express.request, res = express.response) => {
+  middelwares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use("/api", this.router);
+    this.app.use(cors());
+    this.router.use((req, res, next) => {
+      next();
+    });
+    this.router.use(
+      fileUpload({
+        limits: { fileSize: 50 * 1024 * 1024 },
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+      })
+    );
+  }
 
-            try {
-                if (!req.params.name) {
-                    return res.status(400).json({ msg: 'No se ha indicado el nombre de la carpeta.' });
-                }
+  routes() {
+    // ENDPOINT -> CREATE FOLDE IN SHAREPOINT
+    this.router.get(
+      "/create-folder/:name?",
+      (req = express.request, res = express.response) => {
+        try {
+          if (!req.params.name) {
+            return res
+              .status(400)
+              .json({ msg: "No se ha indicado el nombre de la carpeta." });
+          }
 
-                const { name:folderName } = req.params;
+          const { name: folderName } = req.params;
 
-                // invocacion servicio para subir archivo a sharepoint
-                // this.sharepointService.createFolder(folderName)
-                //     .then( result => {
-                //         return res.status(201).json({ msg: result });
-                //     })
-                //     .catch(err => {
-                //         return res.status(422).json({ msg: err });
-                //     });
+          // invocacion servicio para subir archivo a sharepoint
+          // this.sharepointService.createFolder(folderName)
+          //     .then( result => {
+          //         return res.status(201).json({ msg: result });
+          //     })
+          //     .catch(err => {
+          //         return res.status(422).json({ msg: err });
+          //     });
 
-                // proceso en servidor
-                if (fs.existsSync(folderName)){
-                    return res.status(400).json({ msg: `La carpeta ${folderName} ya existe.` });
-                }
-                
-                fs.mkdirSync(folderName, { recursive: true })
-                
-                return fs.existsSync(folderName) ? 
-                    res.status(201).json({ msg: `La carpeta ${folderName} se ha creado correctamente.` }): 
-                    res.status(500).json({ msg: `Ha ocurrido un error en el servidor.` });
-            
-            } catch (error) {
-                res.send({ error: error.code })
-            }
-        });
-            
-        // ENDPOINT -> UPLOAD FILE TO SHAREPOINT
-        this.router.post('/upload-file', (req = express.request, res = express.response) => {
-            try {
-                if (!req.files || !req.files.file) {
-                    return res.status(422).json({ msg: 'No se ha seleccionado ningún fichero.' });
-                }
+          // proceso en servidor
+          if (fs.existsSync(folderName)) {
+            return res
+              .status(400)
+              .json({ msg: `La carpeta ${folderName} ya existe.` });
+          }
 
-                if (!req.body.folder) {
-                    return res.status(422).json({ msg: 'No se ha indicado la carpeta.' });
-                }
-                
-                const { file } = req.files;
-                const { folder:folderName } = req.body;
+          fs.mkdirSync(folderName, { recursive: true });
 
-                // invocacion servicio para subir archivo a sharepoint
-                // this.sharepointService.uploadFile(folderName, file)
-                //     .then( result => {
-                //         return res.status(201).json({ msg: result });
-                //     })
-                //     .catch(err => {
-                //         return res.status(422).json({ msg: err });
-                //     });
+          return fs.existsSync(folderName)
+            ? res.status(201).json({
+                msg: `La carpeta ${folderName} se ha creado correctamente.`,
+              })
+            : res
+                .status(500)
+                .json({ msg: `Ha ocurrido un error en el servidor.` });
+        } catch (error) {
+          res.send({ error: error.code });
+        }
+      }
+    );
 
-                // proceso en servidor
-                if (fs.existsSync(folderName)){
-                    return file.mv(__dirname + '/' + folderName + '/' + file.name)? 
-                        res.status(201).json({ msg: 'Fichero guardado correctamente.' }): 
-                        res.status(400).json({ msg: 'Ha ocurrido un error en el proceso de guardado del fichero.' });
-                } else {
-                    return res.status(400).json({ msg: 'No existe la carpeta indicada.' });
-                }
-            } catch (error) {
-                res.send({ error: error.code })
-            }
-        });
-    }
+    // ENDPOINT -> UPLOAD FILE TO SHAREPOINT
+    this.router.post(
+      "/upload-file",
+      (req = express.request, res = express.response) => {
+        try {
+          if (!req.files || !req.files.file) {
+            return res
+              .status(422)
+              .json({ msg: "No se ha seleccionado ningún fichero." });
+          }
 
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log('Sharepoint Api run in port:', this.port)
-          }) 
-    }
+          if (!req.body.folder) {
+            return res
+              .status(422)
+              .json({ msg: "No se ha indicado la carpeta." });
+          }
+
+          const { file } = req.files;
+          const { folder: folderName } = req.body;
+
+          // invocacion servicio para subir archivo a sharepoint
+          // this.sharepointService.uploadFile(folderName, file)
+          //     .then( result => {
+          //         return res.status(201).json({ msg: result });
+          //     })
+          //     .catch(err => {
+          //         return res.status(422).json({ msg: err });
+          //     });
+
+          // proceso en servidor
+          if (fs.existsSync(folderName)) {
+            return file.mv(__dirname + "/" + folderName + "/" + file.name)
+              ? res.status(201).json({ msg: "Fichero guardado correctamente." })
+              : res.status(400).json({
+                  msg: "Ha ocurrido un error en el proceso de guardado del fichero.",
+                });
+          } else {
+            return res
+              .status(400)
+              .json({ msg: "No existe la carpeta indicada." });
+          }
+        } catch (error) {
+          res.send({ error: error.code });
+        }
+      }
+    );
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Sharepoint Api run in port:", this.port);
+    });
+  }
 }
 
 module.exports = Server;
--- sharepoint-service.js
+++ sharepoint-service.js
@@ -1,94 +1,111 @@
 // https://telefonicacorp-my.sharepoint.com/my?id=/personal/francisco_jimenezmartin_telefonica_com/Documents/prueba&login_hint=francisco.jimenezmartin@telefonica.com
 
 class SharepointService {
-    constructor() {
-        // this.client = new aws.SecretsManager();
-        // this.data = this.client.getSecretValue({ SecretId: '<secret number>' }).promise(); // secret number
-        // this.secret = JSON.parse(this.data.SecretString);
-        this.axios = require('axios');
-    }
+  constructor() {
+    // this.client = new aws.SecretsManager();
+    // this.data = this.client.getSecretValue({ SecretId: '<secret number>' }).promise(); // secret number
+    // this.secret = JSON.parse(this.data.SecretString);
+    this.axios = require("axios");
+  }
 
-    getToken() {
-        return axios.post('https://accounts.accesscontrol.windows.net/<sharepoint resource id>/tokens/OAuth/2', // sharepoint resource id
-            querystring.stringify({
-                grant_type: '<client_credentials>', // client credentials
-                client_id: '<client_id>', // client ID
-                client_secret: this.secret,
-                resource: '<ask your sharepoint person>' // sharepoint person
-            }), {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                }
+  getToken() {
+    return axios
+      .post(
+        "https://accounts.accesscontrol.windows.net/<sharepoint resource id>/tokens/OAuth/2", // sharepoint resource id
+        querystring.stringify({
+          grant_type: "<client_credentials>", // client credentials
+          client_id: "<client_id>", // client ID
+          client_secret: this.secret,
+          resource: "<ask your sharepoint person>", // sharepoint person
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((accessToken) => {
+        return axios
+          .post(
+            "https://telefonicacorp-my.sharepoint.com/my/_api/contextinfo",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
             }
-        ).then( accessToken => {
-            return axios.post('https://telefonicacorp-my.sharepoint.com/my/_api/contextinfo', {}, {
-                headers: {
-                    "Authorization": `Bearer ${accessToken}`,
-    
-                }
-            }).then( result => {
-                return result.data.FormDigestValue
-            }).catch( (error) => {
-                return error;
-            });
-        }).catch( (error) => {
+          )
+          .then((result) => {
+            return result.data.FormDigestValue;
+          })
+          .catch((error) => {
             return error;
+          });
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+
+  createFolder(folderName) {
+    try {
+      const urlSP = `https://telefonicacorp-my.sharepoint.com/my/_api/web/GetFolderByServerRelativeUrl('Documents')/AddFolder(url='${folderName}', overwrite=true)`;
+
+      return this.getToken()
+        .then((result) => {
+          return axios
+            .post(urlSP, folderName, {
+              maxBodyLength: Infinity,
+              maxContentLength: Infinity,
+              headers: {
+                Authorization: `Bearer ${token}`, //  ¿TOKEN?
+                "X-RequestDigest": result,
+              },
+            })
+            .then(() => {
+              return "La carpeta se ha creado correctamente.";
+            })
+            .catch((error) => {
+              return error;
+            });
+        })
+        .catch((error) => {
+          return error;
         });
+    } catch (error) {
+      return error;
     }
+  }
 
-    createFolder(folderName) {
-        try {
-            const urlSP = `https://telefonicacorp-my.sharepoint.com/my/_api/web/GetFolderByServerRelativeUrl('Documents')/AddFolder(url='${folderName}', overwrite=true)`;
-            
-            return this.getToken()
-            .then( ( result ) => {
-                return axios.post(urlSP, folderName, {
-                    maxBodyLength: Infinity,
-                    maxContentLength: Infinity,
-                    headers: {
-                        'Authorization': `Bearer ${token}`, //  ¿TOKEN?
-                        'X-RequestDigest': result
-                    }
-                })
-                .then( () => {
-                    return 'La carpeta se ha creado correctamente.';
-                }).catch( (error) => {
-                    return error;
-                });
-            }).catch( (error) => {
-                return error;
-            });
-        } catch (error) {
-            return error;
-        }
-    }
+  uploadFile(folderName, file) {
+    try {
+      const urlSP = `https://telefonicacorp-my.sharepoint.com/my/_api/web/GetFolderByServerRelativeUrl('Documents')/${folderName}/Add(url='${file.name}', overwrite=true)`;
 
-    uploadFile(folderName, file) {
-        try {
-            const urlSP = `https://telefonicacorp-my.sharepoint.com/my/_api/web/GetFolderByServerRelativeUrl('Documents')/${folderName}/Add(url='${file.name}', overwrite=true)`;
-            
-            return this.getToken()
-            .then( (result) => {
-                return axios.post(urlSP, file, {
-                    maxBodyLength: Infinity,
-                    maxContentLength: Infinity,
-                    headers: {
-                        'Authorization': `Bearer ${token}`, //  ¿TOKEN?
-                        'X-RequestDigest': result
-                    }
-                })
-                .then( () => {
-                    return 'La carpeta se ha creado correctamente.';
-                }).catch( (error) => {
-                    return error;
-                });
-            }).catch( (error) => {
-                return error;
+      return this.getToken()
+        .then((result) => {
+          return axios
+            .post(urlSP, file, {
+              maxBodyLength: Infinity,
+              maxContentLength: Infinity,
+              headers: {
+                Authorization: `Bearer ${token}`, //  ¿TOKEN?
+                "X-RequestDigest": result,
+              },
+            })
+            .then(() => {
+              return "La carpeta se ha creado correctamente.";
+            })
+            .catch((error) => {
+              return error;
             });
-        } catch (error) {
-            return error;
-        }
+        })
+        .catch((error) => {
+          return error;
+        });
+    } catch (error) {
+      return error;
     }
+  }
 }
 
 module.exports = SharepointService;
